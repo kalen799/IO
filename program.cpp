84dd46c12f33beac5f0688286157bc9e9178b90e
KaLeN <kalenalewajski@wp.pl>
2024-11-28 18:44:10 +0100

    Silnia

--- program.cpp
+++ program.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int silnia(int n) {
+    if (n ==0) return 1;
+    return n *silnia(n -1);
+}
+
 int main() {
     int liczba;
     cin >>liczba;
-    cout <<liczba <<endl;
+    cout <<silnia(liczba)<<endl;
     return 0;
 }
 
